ea73ea39779f4cd7c159b41d1e0d7223128735a7
Dawid Iwański <kontakt@raelx.pl>
2022-10-19 10:41:41 +0200

    Przebieg B i C

--- Program.cs
+++ Program.cs
@@ -21,11 +21,11 @@
 // double x=0;
 // double y;
 
-// if(x < -2)
+// if(x <= -2)
 // {
 //   y = -0.5 * x - 3;
 // }
-// else if(x < 1)
+// else if(x <= 1)
 // {
 //   y = ((double)4 / 3) * x + ((double)2 / 3);
 // }
@@ -36,27 +36,43 @@
 
 // Przebieg funkcji B
 
-double x=-6;
+// double x=-5;
+// double y=0;
+
+// if(x <= -5)
+// {
+//     y = 0.5 * x + 0.5;
+// }
+// else if(x <= -2)
+// {
+//     y = (1.44 * x * x) + (10 * x) + 16;
+// }
+// else if (x <= 0)
+// {
+//     y = Math.Sqrt(4 - x * x);
+// }
+// else if (x <= 3)
+// {
+//     y = 2;
+// }
+// else
+// {
+//     y = -((double)3 / 4) * x + 4.25;
+// }
+// Console.WriteLine("X:" + x + " -> Y: " + y);
+
+// Przebieg funkcij C
+
+double x=2;
 double y=0;
 
-if(x < -5)
+if (x <= -4 || x >= 2)
 {
-y = 0.5 * x + 0.5;
-}
-else if(x < -2)
-{
-y = (1.44 * x * x) + (10 * x) + 16;
-}
-else if (x < 0)
-{
-y = Math.Sqrt(4 - x * x);
-}
-else if (x < 3)
-{
-y = 2;
+    y = (-((double)1 / 3) * x) + ((double)2 / 3);
 }
 else
 {
-y = -(3 / 4) * x + 4.25;
+    y = ((double)1 / 3) * (x + 3) * (x - 2);
 }
+
 Console.WriteLine("X:" + x + " -> Y: " + y);
