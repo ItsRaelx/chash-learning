b32c3317a58b5de88bb2e9183402def5ec21e12f
Dawid Iwański <kontakt@raelx.pl>
2022-12-09 12:46:27 +0100

    Regex

--- Program.cs
+++ Program.cs
@@ -1,14 +1,59 @@
-﻿using System;
+﻿// using System;
+// using System.Globalization;
 using System.Text.RegularExpressions;
 
-Console.Write("Enter a string: ");
-string text = Console.ReadLine();
+// namespace App
+// {
+//   class Program
+//   {
+//     static void Main(string[] args)
+//     {
+//       string[] x = new string[args.Length - 2];
+//       string inputDate = args[0] + " " + args[1];
 
-Regex letter = new Regex("[a-z]+", RegexOptions.Compiled | RegexOptions.IgnoreCase);
-Regex number = new Regex("[0-9-]+", RegexOptions.Compiled | RegexOptions.IgnoreCase);
+//       DateTime dt = DateTime.ParseExact(inputDate, "yyyy-MM-dd HH:mm:ss", CultureInfo.InvariantCulture);
 
-Match matchNumber = number.Match(text);
-Console.WriteLine($"value:{matchNumber.Value} position:{matchNumber.Index}");
+//       for(int i = 0; i < args.Length - 2; i++)
+//       {
+//         x[i] = args[i + 2];
+//       }
 
-Match matchLetter = letter.Match(text);
-Console.WriteLine($"value:{matchLetter.Value} position:{matchLetter.Index}");
+//       foreach(string str in x)
+//       {
+//         // Delete numbers from string
+//         string type = Regex.Replace(str, "[+-][0-9]*", "");
+
+//         // Get only numbers from string
+//         int num = int.Parse(Regex.Replace(str, "[^0-9+-]", ""));
+
+//         switch(type)
+//         {
+//           case "mo":
+//             dt = dt.AddMonths(num);
+//             break;
+//           case "m":
+//             dt = dt.AddMinutes(num);
+//             break;
+//           case "s":
+//             dt = dt.AddSeconds(num);
+//             break;
+//           case "h":
+//             dt = dt.AddHours(num);
+//             break;
+//           case "d":
+//             dt = dt.AddDays(num);
+//             break;
+//           case "y":
+//             dt = dt.AddYears(num);
+//             break;
+//         }
+//       }
+//       Console.WriteLine(dt.ToString("yyyy-MM-dd HH:mm:ss"));
+//     }
+//   }
+// }
+
+string text = "https://alias.nazwa.com/js&adj/das$%sadds/sdasd@121/#asdasdasd1232@";
+
+string done = Regex.Match(text, @"https://[a-z]*\.nazwa\.((com)|(pl))[^#]*", RegexOptions.IgnoreCase).Value;
+Console.WriteLine(done);
